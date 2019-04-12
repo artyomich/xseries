d6e21d5e6bfc6e9fb24b037bde7f173f273b76db
Artyom Arifulin <artem.arifulin@enterra-inc.com>
2019-04-12 15:56:04 +0700

    draft

--- css/js/app.js
+++ css/js/app.js
@@ -69,25 +69,6 @@ $(document).ready(function () {
 
         $('#imprint').hide();
     });
-
-    //FAQ accordeon
-    var flatAccordion = function () {
-        var speed = {duration: 600};
-        $('.toggle-content').hide();
-        $('.flat-accordion .toggle-title.active').siblings('.toggle-content').show();
-
-        $('.flat-accordion .toggle-title').on('click', function () {
-            if (!$(this).is('.active')) {
-                $(this).closest('.flat-accordion').find('.toggle-title.active').toggleClass('active').next().slideToggle(speed);
-                $(this).toggleClass('active');
-                $(this).next().slideToggle(speed);
-            } else {
-                $(this).toggleClass('active');
-                $(this).next().slideToggle(speed);
-            }
-        });
-    };
-
 });
 
 $(window).load(function () {
